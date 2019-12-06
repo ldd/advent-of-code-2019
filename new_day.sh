f2de7a79c05257a341ba047aaf2d5f77dad099c2
ldd <leonardoldd@gmail.com>
2019-12-05 23:41:12 -0500

    chore: modify script to download input for each day

--- new_day.sh
+++ new_day.sh
@@ -5,3 +5,10 @@ new_day_path=js/day${day}
 # copy the template to start working on parts 1 & 2
 mkdir -p ${new_day_path}/
 cp ./day_template.js ${new_day_path}/index.js
+
+# download the input if it doesn't exist yet
+input_file=input/day${day}.txt
+if [ ! -f ${input_file} ]; then
+  source .env
+  curl "https://adventofcode.com/2019/day/${day}/input" -H "cookie: session=${ADVENT_SESSION}" -o ${input_file}
+fi
