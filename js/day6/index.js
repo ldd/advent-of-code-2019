7abb7a9a101a5bb2637495a2a79ed20e112e7aed
ldd <leonardoldd@gmail.com>
2019-12-07 10:39:23 -0500

    feat(js): add js solution for part 1 & 2 of day 6 (with help) (#1)

---
+++ js/day6/index.js
@@ -0,0 +1,37 @@
+function parseInput(rawInput = "") {
+  return rawInput
+    .split("\n")
+    .filter(Boolean)
+    .map(entry => entry.split(")"));
+}
+
+const sumReducer = (sum, n) => sum + n;
+
+// to be completely honest, I gave up doing a tree solution
+// this one is roughly based on
+// https://old.reddit.com/r/adventofcode/comments/e6tyva/2019_day_6_solutions/f9tf07s/
+function part1(rawInput) {
+  let A = parseInput(rawInput);
+
+  const valueMap = A.reduce((dic, [l, r]) => ({ ...dic, [r]: l }), {});
+  const count = parent => (parent ? 1 + count(valueMap[parent]) : 0);
+  return A.map(([l, r]) => count(valueMap[r])).reduce(sumReducer);
+}
+
+// still roughly based on the solution I found on reddit
+function part2(rawInput) {
+  const A = parseInput(rawInput);
+
+  const valueMap = A.reduce((dic, [l, r]) => ({ ...dic, [r]: l }), {});
+  const getPath = (child, parent) =>
+    parent ? [child].concat(getPath(parent, valueMap[parent])) : [];
+
+  const youPath = getPath("YOU", valueMap["YOU"]);
+  const sanPath = getPath("SAN", valueMap["SAN"]);
+  return [
+    youPath.filter(p => !sanPath.includes(p)).length,
+    sanPath.filter(p => !youPath.includes(p)).length
+  ].reduce(sumReducer);
+}
+
+module.exports = { part1, part2 };
