0a9c89d3dd3603b047dea0c4b98d2e71f8446d39
ldd <leonardoldd@gmail.com>
2019-12-11 10:07:05 -0500

    chore(input,js): add template and input for day 11

---
+++ js/day11/index.js
@@ -0,0 +1,14 @@
+function parseInput(rawInput = "") {
+  return rawInput;
+}
+
+function part1(rawInput) {
+  const A = parseInput(rawInput);
+  return A;
+}
+function part2(rawInput) {
+  const A = parseInput(rawInput);
+  return A;
+}
+
+module.exports = { part1, part2 };
