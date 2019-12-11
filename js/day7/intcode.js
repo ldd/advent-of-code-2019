6811a03791323dc7ca15d523043cd42db8e59e2c
ldd <leonardoldd@gmail.com>
2019-12-11 10:02:08 -0500

    feat(js): modify day 7's intcode for part 1 day 9

--- js/day7/intcode.js
+++ js/day7/intcode.js
@@ -1,21 +1,39 @@
-function* runIntcodeProgram(A = [], startInput) {
+const IMMEDIATE = "1";
+const RELATIVE = "2";
+
+function* runIntcodeProgram(
+  A = [],
+  startInput,
+  keepOutput = false,
+  relativeBase = 0
+) {
   let j = 0;
   let last = null;
   let input = [startInput];
+  let output = [];
   while (true) {
     for (let i = 0; i < A.length; i += j) {
       s = String(A[i]).padStart(5, 0);
-      const l = s.slice(2, 3) === "1" ? A[i + 1] : A[A[i + 1]];
-      const r = s.slice(1, 2) === "1" ? A[i + 2] : A[A[i + 2]];
+      // 0 is position mode, 1 is immediate mode, 2 is relative mode
+      const [leftMode, rightMode] = [s.slice(2, 3), s.slice(1, 2)];
+      let l = leftMode === IMMEDIATE ? A[i + 1] : A[A[i + 1]];
+      l = leftMode === RELATIVE ? A[A[i + 1] + relativeBase] : l;
+      if (l === undefined) l = 0;
+      let r = rightMode === IMMEDIATE ? A[i + 2] : A[A[i + 2]];
+      r = rightMode === RELATIVE ? A[A[i + 2] + relativeBase] : r;
+      if (r === undefined) r = 0;
+
+      let w = A[i + 3];
+      if (s.slice(0, 1) === RELATIVE) w += relativeBase;
 
       switch (s.slice(3, 5)) {
         case "01": {
-          A[A[i + 3]] = l + r;
+          A[w] = l + r;
           j = 4;
           break;
         }
         case "02": {
-          A[A[i + 3]] = l * r;
+          A[w] = l * r;
           j = 4;
           break;
         }
@@ -24,7 +42,9 @@ function* runIntcodeProgram(A = [], startInput) {
             const nextInput = yield;
             input = [nextInput];
           }
-          A[A[i + 1]] = input[0];
+          w = A[i + 1];
+          if (leftMode === RELATIVE) w += relativeBase;
+          A[w] = input[0];
           input = input.slice(1);
           j = 2;
           break;
@@ -32,6 +52,7 @@ function* runIntcodeProgram(A = [], startInput) {
         case "04": {
           j = 2;
           last = l;
+          if (keepOutput) output = output.concat(last);
           yield l;
           break;
         }
@@ -46,16 +67,22 @@ function* runIntcodeProgram(A = [], startInput) {
           break;
         }
         case "07": {
-          A[A[i + 3]] = l < r ? 1 : 0;
+          A[w] = l < r ? 1 : 0;
           j = 4;
           break;
         }
         case "08": {
-          A[A[i + 3]] = l === r ? 1 : 0;
+          A[w] = l === r ? 1 : 0;
           j = 4;
           break;
         }
+        case "09": {
+          relativeBase += l;
+          j = 2;
+          break;
+        }
         case "99":
+          if (keepOutput) return output.join(",");
           return last;
         default:
           j = Infinity;
