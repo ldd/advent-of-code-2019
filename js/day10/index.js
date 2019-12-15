5ebfbaeda6e89e29605cb6de91079f23fc3d6a2a
ldd <leonardoldd@gmail.com>
2019-12-15 11:39:06 -0500

    feat(js): add js solution for part 2 of day 10 (finally :D)

--- js/day10/index.js
+++ js/day10/index.js
@@ -7,34 +7,68 @@ function parseInput(rawInput = "") {
     );
 }
 
-function part1(rawInput) {
-  const A = parseInput(rawInput);
-  const asteroids = A.flatMap(line =>
-    line.filter(({ hasAsteroid }) => hasAsteroid)
-  );
-  const maxAsteroid = asteroids
+function getAsteroids(A = []) {
+  return A.flatMap(line => line.filter(({ hasAsteroid }) => hasAsteroid));
+}
+function getAngle(a, b) {
+  const dy = b.y - a.y;
+  const dx = b.x - a.x;
+
+  // NOTICE that we need to add Math.PI/2 to start at the right angle
+  let angle = Math.atan2(dy, dx) + Math.PI / 2;
+  if (angle < 0) angle += Math.PI * 2;
+  const key = `${angle}`;
+  return key;
+}
+function getMaxAsteroid(asteroids) {
+  return asteroids
     .map(asteroid =>
       asteroids.reduce(
-        (dic, { x, y }) => {
-          if (x === asteroid.x && y === asteroid.y) return dic;
-          const dy = y - asteroid.y;
-          const dx = x - asteroid.x;
-          const angle = Math.atan2(dx, dy);
-          const key = `${angle},${Math.sign(dx)},${Math.sign(dy)}`;
+        (dic, otherAsteroid) => {
+          if (otherAsteroid.x === asteroid.x && otherAsteroid.y === asteroid.y)
+            return dic;
+          const key = getAngle(asteroid, otherAsteroid);
 
-          if (!dic[key]) dic.count += 1;
-          return { ...dic, [key]: true, count: dic.count };
+          const count = !dic[key] ? dic.count + 1 : dic.count;
+          let angleValues = dic[key] || [];
+          angleValues = [...angleValues, [otherAsteroid.x, otherAsteroid.y]];
+          return { ...dic, [key]: angleValues, count };
         },
-        { count: 0 }
+        { count: 0, position: { x: asteroid.x, y: asteroid.y } }
       )
     )
     .reduce((max, next) => (next.count < max.count ? max : next));
+}
+
+function part1(rawInput) {
+  const A = parseInput(rawInput);
+  const asteroids = getAsteroids(A);
+  const maxAsteroid = getMaxAsteroid(asteroids);
   return maxAsteroid.count;
 }
 
 function part2(rawInput) {
   const A = parseInput(rawInput);
-  return A;
+  const asteroids = getAsteroids(A);
+  const groupedAsteroids = getMaxAsteroid(asteroids);
+  const { position } = groupedAsteroids;
+  const { x, y } = position;
+
+  const sortedAsteroids = Object.entries(groupedAsteroids)
+    .filter(([key]) => key !== "count" && key !== "position")
+    .sort(([aKey], [bKey]) => +aKey - +bKey);
+
+  const [, pickedValues] = sortedAsteroids[199];
+
+  if (pickedValues.length !== 1) {
+    pickedValues.sort((a, b) => {
+      const dA = Math.abs(a[0] - x) + Math.abs(a[1] - y);
+      const dB = Math.abs(b[0] - x) + Math.abs(b[1] - y);
+      return dA - dB;
+    });
+  }
+  const [pickedX, pickedY] = pickedValues[0];
+  return pickedX * 100 + pickedY;
 }
 
 module.exports = { part1, part2 };
