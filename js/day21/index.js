04c489465b44d5192cf216e244e1ab0fe8ffca2b
ldd <leonardoldd@gmail.com>
2019-12-23 12:12:36 -0500

    and part 2 for 21 done

--- js/day21/index.js
+++ js/day21/index.js
@@ -4,9 +4,13 @@ function parseInput(rawInput = "") {
   return rawInput.split(",").map(Number);
 }
 
-function getResultFromProgram(A = [], programArray = []) {
+function getResultFromProgram(A = [], programArray = [], keepOutput = false) {
   let i = 0;
-  const instance = runIntcodeProgram([...A], programArray[i].shift(), false);
+  const instance = runIntcodeProgram(
+    [...A],
+    programArray[i].shift(),
+    keepOutput
+  );
   let result = { value: undefined, done: false };
   while (result.done === false) {
     const oldValue = result.value;
@@ -31,18 +35,8 @@ function parseASCII(input) {
     .join("");
 }
 
-function part1(rawInput) {
-  const A = parseInput(rawInput);
-  const rawProgram = `
-  NOT A J
-  NOT B T
-  OR T J
-  NOT C T
-  OR T J
-  AND D J
-  WALK
-  `;
-  const program = rawProgram
+function encodeASCII(input) {
+  return input
     .split("\n")
     .map(s =>
       `${s}\n`
@@ -51,13 +45,47 @@ function part1(rawInput) {
         .map(e => e.charCodeAt(0))
     )
     .filter(s => s.length > 0);
-  const a = getResultFromProgram(A, program);
-  return a;
+}
+
+function part1(rawInput) {
+  const A = parseInput(rawInput);
+  const rawProgram = `
+  OR A T
+  AND B T
+  AND C T
+  NOT T J
+
+  NOT T T
+
+  AND D T
+  AND T J
+  WALK
+  `;
+  const program = encodeASCII(rawProgram);
+  return getResultFromProgram(A, program);
 }
 
 function part2(rawInput) {
-  // const A = parseInput(rawInput);
-  return null;
+  const A = parseInput(rawInput);
+
+  // TODO: those OR in the middle were added
+  // by trial and error to make sure they pass the tests
+  // (it is possible that they are not generic enough)
+  const rawProgram = `
+  OR A T
+  AND B T
+  AND C T
+  NOT T J
+  
+  OR H T
+  OR E T
+
+  AND D T
+  AND T J
+  RUN
+  `;
+  const program = encodeASCII(rawProgram);
+  return getResultFromProgram(A, program);
 }
 
 module.exports = { part1, part2 };
