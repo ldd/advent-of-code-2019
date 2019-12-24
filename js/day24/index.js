19dc1cd2417e4cffa05890c03eec227fbfbb3d3c
ldd <leonardoldd@gmail.com>
2019-12-24 13:20:53 -0500

    clearly closer in part 2

--- js/day24/index.js
+++ js/day24/index.js
@@ -7,28 +7,71 @@ function parseInput(rawInput = "") {
 
 const ROWS = 5;
 const COLS = 5;
+const MID = 2;
 
-function getNeighbours(x, y, rows = ROWS, cols = COLS) {
-  return [
+const MAX_DEPTH = 5;
+const MIN_DEPTH = -5;
+function getNeighbours(x, y, depth, rows = ROWS, cols = COLS) {
+  const close = [
     [x - 1, y],
     [x + 1, y],
     [x, y - 1],
     [x, y + 1]
   ].filter(([i, j]) => i >= 0 && i < rows && j >= 0 && j < cols);
+
+  if (depth !== undefined) {
+    // never fill center stage
+    if (x === MID && y === MID) return [];
+    if (MIN_DEPTH < depth) {
+      if (x === 0) return close.concat([[MID - 1, MID, depth - 1]]);
+      if (x === COLS - 1) return close.concat([[MID + 1, MID, depth - 1]]);
+      if (y === 0) return close.concat([[MID, MID - 1, depth - 1]]);
+      if (y === ROWS - 1) return close.concat([[MID, MID + 1, depth - 1]]);
+    }
+    if (x === MID - 1 && y === MID) {
+      const firstLine = Array(ROWS)
+        .fill()
+        .map((_, j) => [0, j, depth + 1]);
+      return close.concat(firstLine);
+    }
+    if (x === MID + 1 && y === MID) {
+      const firstLine = Array(ROWS)
+        .fill()
+        .map((_, j) => [COLS - 1, j, depth + 1]);
+      return close.concat(firstLine);
+    }
+    if (x === MID && y === MID - 1) {
+      const firstLine = Array(COLS)
+        .fill()
+        .map((_, i) => [i, 0, depth + 1]);
+      return close.concat(firstLine);
+    }
+    if (x === MID && y === MID + 1) {
+      const firstLine = Array(COLS)
+        .fill()
+        .map((_, i) => [i, ROWS - 1, depth + 1]);
+      return close.concat(firstLine);
+    }
+  }
+
+  return close;
+}
+
+function bugRules(entry, bugNearCount) {
+  // you are bug, find out if you die
+  if (entry === "#") {
+    if (bugNearCount === 1) return "#";
+    return ".";
+  }
+  if (bugNearCount === 1 || bugNearCount === 2) return "#";
+  return ".";
 }
 
 function getNextState(A, x, y) {
   const isBug = ([i, j]) => A[i][j] === "#";
-  const bugNeighbourCount = getNeighbours(x, y).filter(isBug);
-  const l = bugNeighbourCount.length;
-
-  // you are bug, find out if you die
-  if (A[x][y] === "#") {
-    if (l === 1) return "#";
-    return ".";
-  }
-  if (l === 1 || l === 2) return "#";
-  return ".";
+  const bugNeighbours = getNeighbours(x, y).filter(isBug);
+  const l = bugNeighbours.length;
+  return bugRules(A[x][y], l);
 }
 
 function biodiversityScore(A) {
@@ -41,10 +84,14 @@ function tick(A = []) {
   return A.map((row, i) => row.map((_, j) => getNextState(A, i, j)));
 }
 
+function copyState(state = []) {
+  return JSON.parse(JSON.stringify(state));
+}
+
 function part1(rawInput) {
   const input = parseInput(rawInput);
 
-  let state = JSON.parse(JSON.stringify(input));
+  let state = copyState(input);
   const scoreDic = {};
   for (;;) {
     const score = biodiversityScore(state);
@@ -55,8 +102,35 @@ function part1(rawInput) {
     state = tick(state);
   }
 }
+
+function getNextStateDeep(states, x, y, depth) {
+  const isBug = ([i, j]) => states[0][i][j] === "#";
+  const bugNeighbours = getNeighbours(x, y).filter(isBug);
+  const l = bugNeighbours.length;
+  return bugRules(states[0][x][y], l);
+}
+
+function tickDeep(states = [], depth) {
+  const state = states[depth - MIN_DEPTH];
+  return state.map((row, i) =>
+    row.map((_, j) => getNextStateDeep(states, i, j, depth))
+  );
+}
+
 function part2(rawInput) {
-  return null;
+  rawInput = `....#
+#..#.
+#.?##
+..#..
+#....`;
+  const input = parseInput(rawInput);
+  let states = Array(11)
+    .fill()
+    .map(() => copyState(input));
+  for (let i = 0; i < 4; i += 1) {
+    states = states.map((state, depth) => tickDeep(states, depth + MIN_DEPTH));
+  }
+  return states;
 }
 
 module.exports = { part1, part2 };
